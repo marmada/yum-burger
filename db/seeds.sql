b72eddb26863a8eba67cc4e9bc90445f6b5faa95
Miriam Armada <miriam_armada@yahoo.com>
2018-05-07 16:47:20 -0700

    Update

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+USE burger_db;
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Bacon Cheese Burger'), ('Double Bacon Cheese Burger'), ('Turkey Ranch Avocado Burger'), ('Southwestern Cheddar Chicken Burger');
