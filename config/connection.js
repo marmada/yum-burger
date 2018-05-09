e43514210e6300de226914c21caa6246a7268806
Miriam Armada <miriam_armada@yahoo.com>
2018-05-09 16:35:32 -0700

    remove node modules

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,27 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "4390",
-  database: "burger_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '4390',
+    database: 'burger_db',
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 // Export connection for our ORM to use.
